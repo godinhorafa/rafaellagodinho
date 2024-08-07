a39d89c62d5eebc601c2cf9d9dc138d73f835c70
Rafaella Godinho <rafaella.oliveira@fadami.com.br>
2024-08-07 16:49:09 -0300

    update

---
+++ components/Experience.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { workExperience } from "@/data";
+import { Button } from "./ui/MovingBorders";
+
+const handleDownloadPDF = () => {
+  const link = document.createElement("a");
+  link.href = "/curriculo_rafaellagodinho_2024.pdf";
+  link.download = "Rafaella_Godinho_Curriculo.pdf";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+};
+
+const Experience = () => {
+  return (
+    <div className="py-20 w-full" id="experience">
+      <h1 className="heading">
+        <span className="text-purple">Experiência</span>
+      </h1>
+      <div className="w-full mt-12 grid lg:grid-cols-4 grid-cols-1 gap-10 mb-4">
+        {workExperience.map((card) => (
+          <Button
+            key={card.id}
+            duration={Math.floor(Math.random() * 10000) + 10000}
+            borderRadius="1.75rem"
+            style={{
+              background: "rgb(4,7,29)",
+              backgroundColor:
+                "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+              borderRadius: `calc(1.75rem* 0.96)`,
+            }}
+            className="flex-1 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+          >
+            <div className="flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2">
+              <div className="lg:ms-5">
+                <h1 className="text-start text-xl md:text-2xl font-bold">
+                  {card.title}
+                </h1>
+                <p className="text-start text-white-100 mt-3 font-semibold">
+                  {card.desc}
+                </p>
+              </div>
+            </div>
+          </Button>
+        ))}
+      </div>
+      <div className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl mt-14">
+        Para mais detalhes, você pode baixar meu currículo ao clicar{" "}
+        <span
+          className="text-purple cursor-pointer"
+          onClick={handleDownloadPDF}
+        >
+          aqui
+        </span>
+        .
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
---
+++ data/index.ts
@@ -0,0 +1,126 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Carepulse (Healthcare)",
+    des: "A healthcare patient management application that allows patients to easily register, book, and manage their appointments with doctors, featuring administrative tools for scheduling, confirming, and canceling appointments, along with SMS notifications, all built using Next.js.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/fm.svg"],
+    link: "https://github.com/godinhorafa/carepulse",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Desenvolvedor Frontend",
+    desc: "Responsável pelo desenvolvimento frontend de uma plataforma avançada de gestão de pagamentos de pedágio, definindo arquitetura, tecnologias e ferramentas para um site moderno e eficiente.",
+    className: "md:col-span-2",
+  },
+  {
+    id: 2,
+    title: "Desenvolvedor Freelancer",
+    desc: "Liderança no design e implementação de interface de usuário utilizando a stack MERN e Material UI.",
+    className: "md:col-span-2",
+  },
+  {
+    id: 3,
+    title: "Analista de Desenvolvimento",
+    desc: "Liderança no desenvolvimento e manutenção do portal de vendas, com foco especial na segurança da informação, acessibilidade e performance. Implementei soluções inovadoras centradas na eficácia, contribuindo para uma experiência de usuário aprimorada e para a integridade do sistema em um ambiente de alta demanda.",
+    className: "md:col-span-2",
+  },
+  {
+    id: 4,
+    title: "Programador de Sistemas",
+    desc: "Desenvolvi formulários de pesquisa e extensão, integrando eficientemente sistemas de gerenciamento de helpdesk. Implementei sistemas de controle robustos para funcionários e alunos, facilitando o gerenciamento de recursos e melhorando a eficiência operacional.",
+    className: "md:col-span-2",
+  },
+  {
+    id: 5,
+    title: "Estagiário de TI",
+    desc: "Suporte técnico para alunos e professores, resolvendo problemas de acesso ao site e auxiliando em questões relacionadas a bolsas de estudo.",
+    className: "md:col-span-2",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/godinhorafa",
+  },
+  {
+    id: 2,
+    img: "/link.svg",
+    link: "https://br.linkedin.com/in/rafaella-godinho",
+  },
+];
