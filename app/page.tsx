077217e9002fbe44da91a0b83e41af4f8e17277d
Rafaella Godinho <rafaellagodinho@outlook.com>
2025-01-21 10:03:24 -0300

    analyticsw config for vercel

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,13 @@ import Footer from "@/components/Footer";
 import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import { Analytics } from "@vercel/analytics/react";
 
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <Analytics />
         <FloatingNav navItems={navItems} />
         <Hero />
         <Experience />
